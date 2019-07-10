55ad49fbfcf4079779c659898b01c838861cf730
Najabi <35067475+Najabi@users.noreply.github.com>
2019-07-10 21:22:45 +0200

    [RED] Tests fallan

---
+++ EjercicioFizz/ClaseFizz.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+//Escribe un programa que imprima los número del 1 al 100, pero aplicando las siguientes normas: 
+//Devuelve Fizz si el número es = 3 o divisible por 3. 
+//Devuelve Buzz si el número es = 5 o divisible por 5. 
+//Devuelve FizzBuzz si el número es divisible por 3 y por 5. 
+
+namespace EjercicioFizz
+{
+    public class ClaseFizz
+    {
+        public static string Comprobar(int numero)
+        {
+            throw new Exception("No implementado");
+        }
+    }
+}
--- NUnitTestProject1/UnitTest1.cs
+++ NUnitTestProject1/UnitTest1.cs
@@ -1,4 +1,10 @@
 using NUnit.Framework;
+using EjercicioFizz;
+
+//Escribe un programa que imprima los número del 1 al 100, pero aplicando las siguientes normas: 
+//Devuelve Fizz si el número es = 3 o divisible por 3. 
+//Devuelve Buzz si el número es = 5 o divisible por 5. 
+//Devuelve FizzBuzz si el número es divisible por 3 y por 5. 
 
 namespace Tests
 {
@@ -10,9 +16,110 @@ namespace Tests
         }
 
         [Test]
-        public void Test1()
+        public void TestFizz21()
         {
-            Assert.Pass();
+            //Arrange
+            int numero = 21;
+            string resultado = "Fizz";
+            string resultadoComprobar;
+
+            //Act
+            resultadoComprobar = ClaseFizz.Comprobar(numero);
+
+            //Asert
+            Assert.Equals(resultado, resultadoComprobar);
         }
+
+        [Test]
+        public void TestFizz3()
+        {
+            //Arrange
+            int numero = 3;
+            string resultado = "Fizz";
+            string resultadoComprobar;
+
+            //Act
+            resultadoComprobar = ClaseFizz.Comprobar(numero);
+
+            //Asert
+            Assert.Equals(resultado, resultadoComprobar);
+        }
+
+        [Test]
+        public void TestFizz5()
+        {
+            //Arrange
+            int numero = 5;
+            string resultado = "Buzz";
+            string resultadoComprobar;
+
+            //Act
+            resultadoComprobar = ClaseFizz.Comprobar(numero);
+
+            //Asert
+            Assert.Equals(resultado, resultadoComprobar);
+        }
+
+        [Test]
+        public void TestFizz35()
+        {
+            //Arrange
+            int numero = 35;
+            string resultado = "Buzz";
+            string resultadoComprobar;
+
+            //Act
+            resultadoComprobar = ClaseFizz.Comprobar(numero);
+
+            //Asert
+            Assert.Equals(resultado, resultadoComprobar);
+        }
+
+        [Test]
+        public void TestFizz30()
+        {
+            //Arrange
+            int numero = 30;
+            string resultado = "FizzBuzz";
+            string resultadoComprobar;
+
+            //Act
+            resultadoComprobar = ClaseFizz.Comprobar(numero);
+
+            //Asert
+            Assert.Equals(resultado, resultadoComprobar);
+        }
+
+        [Test]
+        public void TestFizz32()
+        {
+            //Arrange
+            int numero = 32;
+            string resultado = "32";
+            string resultadoComprobar;
+
+            //Act
+            resultadoComprobar = ClaseFizz.Comprobar(numero);
+
+            //Asert
+            Assert.Equals(resultado, resultadoComprobar);
+        }
+
+        [Test]
+        public void TestFizz80()
+        {
+            //Arrange
+            int numero = 80;
+            string resultado = "FizzBuzz";
+            string resultadoComprobar;
+
+            //Act
+            resultadoComprobar = ClaseFizz.Comprobar(numero);
+
+            //Asert
+            Assert.Equals(resultado, resultadoComprobar);
+        }
+
+
     }
 }
